af67d3ba30e79717ba5399675a515673ecb4617a
Leo Villalba <leomarcelovillalba@gmail.com>
2022-11-02 21:54:06 +0800

    fixed lint error

--- app/app/tests.py
+++ app/app/tests.py
@@ -11,13 +11,10 @@ class CalcTest(SimpleTestCase):
 
     def test_add_numbers(self):
         """Test adding numbers together."""
-        res = calc.add(5,6)
-        
+        res = calc.add(5, 6)
         self.assertEqual(res, 11)
 
     def test_subtract_numbers(self):
         """Test subtracting numbers together."""
         res = calc.subtract(10, 15)
-
         self.assertEqual(res, 5)
-        
