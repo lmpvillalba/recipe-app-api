7ad6ad0c8657c43ee7f1b4cce1a91037fe9af992
Leo Villalba <leomarcelovillalba@gmail.com>
2022-11-08 15:44:32 +0800

    added recipe api

--- app/user/views.py
+++ app/user/views.py
@@ -25,8 +25,8 @@ class CreateTokenView(ObtainAuthToken):
 class ManageUserView(generics.RetrieveUpdateAPIView):
     """Manage the authenticated user."""
     serializer_class = UserSerializer
-    authentication_class = [authentication.TokenAuthentication]
-    permission_class = [permissions.IsAuthenticated]
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_object(self):
         """Retrieve and return the authenticated user."""
